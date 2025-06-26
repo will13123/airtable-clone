7117e54f2736b2b2db1b729e9a813c66c40cc0bd
William Mei <william.mei2016@gmail.com>
2025-06-26 22:59:45 +1000

    Removed post import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 import Link from "next/link";
 
-import { LatestPost } from "~/app/_components/post";
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 import Image from "next/image"
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
   const session = await auth();
 
   if (session?.user) {
-    void api.post.getLatest.prefetch();
     if (session?.user) {
       redirect("/dashboard");
     }
